d46afccd63cd6accfcfd188a846dd0141612aff1
Tasneem08 <tasneem08@ufl.edu>
2017-09-11 22:25:16 -0400

    Adding necessary files
    did a mix new bitcoinminer

---
+++ bitcoinminer/lib/bitcoinminer.ex
@@ -0,0 +1,18 @@
+defmodule Bitcoinminer do
+  @moduledoc """
+  Documentation for Bitcoinminer.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bitcoinminer.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
