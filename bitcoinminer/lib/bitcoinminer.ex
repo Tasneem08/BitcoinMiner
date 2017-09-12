1f2d2e812e7fd45b496ceffc87dcf92b52580bde
Mugdha <mathkarmugdha@gmail.com>
2017-09-12 17:41:36 -0400

    getKZeroes

--- bitcoinminer/lib/bitcoinminer.ex
+++ bitcoinminer/lib/bitcoinminer.ex
@@ -4,21 +4,37 @@ defmodule Bitcoinminer do
   def getRandomStr do
   len =10
   salt = :crypto.strong_rand_bytes(len) |> Base.encode64 |> binary_part(0, len)
-  IO.puts "#{salt}"
   "mmathkar" <> salt
   end
 
+<<<<<<< HEAD
   def calculateSha(inputStr,k) do
+  hashVal=:crypto.hash(:sha256,inputStr) |> Base.encode16(case: :lower)
+  String.starts_with?(hashVal, k)
+  end
+
+  
+  def mainMethod(k) do
+  comparator= String.duplicate("0",k)
+  getRandomStr()|>calculateSha(comparator)
+=======
+  def validateHash(inputStr,k) do
   comparator = getKZeroes(k)
   hashVal=:crypto.hash(:sha256,inputStr) |> Base.encode16(case: :lower)
-  String.starts_with?(hashVal, comparator)
+  bool = String.starts_with?(hashVal, comparator)
+  if bool == true do
+    IO.puts "#{inputStr}    #{hashVal}"
+  end
   end
 
   def getKZeroes(k) do
-   String.duplicate("0",k)
+   String.duplicate("0", k)
   end
+  
   def mainMethod(k) do
-  getRandomStr()|>calculateSha(k)
+  getRandomStr()|>validateHash(k)
+  mainMethod(k)
+>>>>>>> 7980950dfb2148dafc7df947683892e126293a6a
   end
-end
 
+end
