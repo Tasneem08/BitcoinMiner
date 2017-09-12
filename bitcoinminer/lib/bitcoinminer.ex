7bae304c541a0b887a132901cd584c42da8ae8a5
Tasneem08 <tasneem08@ufl.edu>
2017-09-12 19:47:54 -0400

    Refactoring. Calling k string once.

--- bitcoinminer/lib/bitcoinminer.ex
+++ bitcoinminer/lib/bitcoinminer.ex
@@ -1,40 +1,37 @@
 defmodule Bitcoinminer do
 
   def main(args) do
-    List.first(args) |> String.to_integer() |> mainMethod()
+    List.first(args) |> String.to_integer() |> getKZeroes() |> mainMethod()
+  end
+
+  def getKZeroes(k) do
+   String.duplicate("0", k)
+  end
+
+  def mainMethod(k) do
+  getRandomStr()|>validateHash(k)
+  mainMethod(k)
   end
 
   def getRandomStr do
-  len =10
+  len =20
   salt = :crypto.strong_rand_bytes(len) |> Base.encode64 |> binary_part(0, len)
   "mmathkar" <> salt
   end
 
-  def validateHash(inputStr,k) do
-  comparator = getKZeroes(k)
+  def validateHash(inputStr,comparator) do
   hashVal=:crypto.hash(:sha256,inputStr) |> Base.encode16(case: :lower)
   bool = String.starts_with?(hashVal, comparator)
   if bool == true do
     
     #mapBool=map.has_key?(inputStr)
     #if mapBool == false do
-    isPresent = Map.has_key?(map,inputStr)
-    if isPresent == false do
+ 
     map=%{inputStr=>hashVal}
+       isPresent = Map.has_key?(map,inputStr)
+    if isPresent == true do
     IO.puts "#{inputStr}    #{hashVal}"
     end
-    
-    
   end
   end
-
-  def getKZeroes(k) do
-   String.duplicate("0", k)
-  end
-  
-  def mainMethod(k) do
-  getRandomStr()|>validateHash(k)
-  mainMethod(k)
-  end
-
 end
